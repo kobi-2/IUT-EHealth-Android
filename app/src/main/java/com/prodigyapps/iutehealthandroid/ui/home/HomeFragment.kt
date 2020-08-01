66ebda984a0484b6cff39aafeb2a8e29a89a9066
kobi-2 <mueeze.nhpg@gmail.com>
2020-08-02 02:09:36 +0600

    MOST IMPORTANT: added zoom meeting call invoke by invoking url when pressed dummy button on home tab. OTHERS: added coupls of zoom api library while trying, probably don't need them.

--- app/src/main/java/com/prodigyapps/iutehealthandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/prodigyapps/iutehealthandroid/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.prodigyapps.iutehealthandroid.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +10,10 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.prodigyapps.iutehealthandroid.R
+
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +31,23 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        root.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+
+//            this is meeting link...probably unique
+//            val meetingLink: String = "https://zoom.us/j/98182761298?pwd=SmtHU2FEZHA2a0h0T1BValYweEZRZz09"
+//            this is using personal id
+//            val meetingLink: String = "https://bdren.zoom.us/s/9016922344"
+//            this is personal meeting link
+            val meetingLink: String = "https://zoom.us/j/9016922344?pwd=YXNnZGwwRHQyMVZoSnpWcnVLWEpQdz09"
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(meetingLink))
+
+            startActivity(browserIntent)
+        }
+
         return root
     }
 }
